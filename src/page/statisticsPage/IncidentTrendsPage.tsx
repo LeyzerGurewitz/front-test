f8c2da796c384472559deb4696e6190e5db928fd
LeyzerGurewitz <leyzer.3770@gmail.com>
2024-12-25 12:01:28 +0200

    delete type react

--- src/page/statisticsPage/IncidentTrendsPage.tsx
+++ src/page/statisticsPage/IncidentTrendsPage.tsx
@@ -1,5 +1,4 @@
 import { Box } from "@mui/material";
-import React from "react";
 import IncidentTrends from "../../components/statisticsComponents/IncidentTrends";
 
 export const IncidentTrendsPage = () => {
